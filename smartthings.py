5c8075ace234f5b4ee985f3b16c82104017841a3
karel2324 <160608332+karel2324@users.noreply.github.com>
2024-05-12 17:52:26 +0200

    Add files via upload

--- smartthings.py
+++ smartthings.py
@@ -1,61 +1,40 @@
+import sys
 import pandas as pd
 import datetime as dt
 import glob
-import click
-from home_messages_db import HomeMessagesDB
+import home_messages_db as hm
+import sqlalchemy as sa
+import os
 
+def smart(file):
+    data = pd.read_csv (str(file), sep = '\t')   #read in the files
+    data = data.drop('attribute', axis=1) #Selecting columns
+    data = data.drop_duplicates() #removing duplicates
+    data.rename(columns={"epoch":"time"},inplace=True)
+    data["time"] = pd.to_datetime(data.time).dt.tz_localize(None) # Remove time zones
+    data["time"] = (data['time'] - dt.datetime(1970,1,1)).dt.total_seconds()  #Change to epoch time
+    data = data.reset_index(drop=True)  #Remove index from dataframe
+    return data
 
-def read_tsv_files(directory):
-    tsv_files = glob.glob(directory + "/*.tsv")  # Get a list of all .tsv files in the directory
-    combined_df = pd.DataFrame()  # Initialize an empty DataFrame to store combined data
-    for file in tsv_files:
-        df = pd.read_csv(file, sep='\t')  # Read the file as a DataFrame with tab separator
-        combined_df = pd.concat([combined_df, df], ignore_index=True)  # Concatenate the DataFrame
-    return combined_df
+used = False #whether or not the function table is used
+while used == False:
+    userinput = input("What would you like to do with this tool? Use -h or --help for a description of the tool. \n") #get user input
+    if userinput == "-h" or userinput == "--help":
+        print("Usage:\n smartthings.py [OPTIONS] smartthings-2022-01-01-2022-09-22.tsv [...]\n")
+        print("Output options: \n -d DBURL insert into the project database (DBURL is a SQLALchemy database URL) \n")
+    elif "smartthings.py -d " in userinput: # else if input is of type: tool -d DBURL filename
+        li = list(userinput.split(" "))
+        path = "efds/smartthings/"+li[3]
+        name = li[0].replace('.py', '')
+        for filepath in glob.glob(path):
+            df = smart(filepath)
+            db = hm.HomeMessagesDB(li[2]) #create instance
+            db.connect() #connecting
+            db.add(df, name) #adding dataframe
+        used = True
+    else:
+        print("We do not understand what you are asking \n")
+        
 
-
-directory_path = "C:\\Users\\HP\\Desktop\\Python\\Group_assignment\\data\\smartthings\\smarthings.tsv"
-
-combined_dataframe = read_tsv_files(directory_path)
-
-# Now you have a single DataFrame containing data from all TSV files in the directory
-combined_dataframe
-
-
-def remove_duplicates(dataframe):
-    # Remove duplicates based on all columns
-    deduplicated_df = dataframe.drop_duplicates()
-    return deduplicated_df
-
-
-deduplicated_dataframe = remove_duplicates(combined_dataframe)
-deduplicated_dataframe
-
-deduplicated_dataframe=deduplicated_dataframe.rename(columns={'epoch':'time'})
-deduplicated_dataframe
-
-@click.command()
-@click.option('-d', '--dburl', required=True, help='Insert into the project database (DBURL is a SQLAlchemy database URL)')
-@click.argument('files', nargs=-1, type=click.Path(exists=True))
-def insert_data(dburl, files):
-    """Insert data from SmartThings files into the database."""
-    db = HomeMessagesDB(dburl)
-    for file in files:
-        try:
-            # Read data from file
-            combined_dataframe = read_tsv_files(file)
-
-            # Remove duplicates
-            deduplicated_dataframe = remove_duplicates(combined_dataframe)
-
-            # Rename columns
-            deduplicated_dataframe = deduplicated_dataframe.rename(columns={'epoch': 'time'})
-
-            # Insert data into the database
-            db.insert_data(deduplicated_dataframe)
-            click.echo(f"Data from {file} inserted into the database.")
-        except Exception as e:
-            click.echo(f"Error inserting data from {file}: {str(e)}", err=True)
-
-if __name__ == '__main__':
-    insert_data()
+db.dup(name) #Remove duplicates from the table
+    
